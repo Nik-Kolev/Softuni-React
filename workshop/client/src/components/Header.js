5b7dbb961034a9123e9aea54a8b1fe58746a0254
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-09-14 19:56:42 +0300

    Project User List Workshop initializing + first component

---
+++ workshop/client/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header class='header'>
+            <div class='logo'>
+                <span class='course'>React Course - June 2022</span>
+                <span class='description'>User List Demo</span>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
