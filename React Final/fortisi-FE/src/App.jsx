241766ed33f6ad59c73ffe34c7340db974368796
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-11-14 20:57:57 +0200

    added zustand and react-toastify and updated App with toast container

--- React Final/fortisi-FE/src/App.jsx
+++ React Final/fortisi-FE/src/App.jsx
@@ -8,36 +8,50 @@ import Register from './Components/Authentication/Register/Register';
 import Login from './Components/Authentication/Login/Login';
 import SomethingWentWrong from './Components/404/404';
 import PopularProducts from './Components/Catalog/PopularProducts/PopularProducts';
-import Toasty from './components/ToastNotifications/Toasty';
-import { ErrorProvider } from './context/ErrorContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import Toasty from './components/ToastNotifications/Toasty';
+// import { ErrorProvider } from './context/ErrorContext';
 
 function App() {
     return (
         <>
             <NavBar />
-            <ErrorProvider>
-                <Routes>
-                    <Route
-                        path='/'
-                        element={
-                            <>
-                                <Carousel />
-                                <PopularProducts />
-                            </>
-                        }
-                    />
-                    {/* <Route path='/catalog' element={<Catalog />} /> */}
-                    {/* <Route path='/about-us' element={<AboutUs />} /> */}
-                    <Route path='/blog' element={<Blog />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
-                    {/* <Route path='/logout' element={<Logout />} /> */}
-                    {/* <Route path='/profile' element={<Profile />} /> */}
-                    {/* <Route path='/shop' element={<ShopCart />} /> */}
-                    <Route path='*' element={<SomethingWentWrong />} />
-                </Routes>
-                <Toasty />
-            </ErrorProvider>
+            {/* <ErrorProvider> */}
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <>
+                            <Carousel />
+                            <PopularProducts />
+                        </>
+                    }
+                />
+                {/* <Route path='/catalog' element={<Catalog />} /> */}
+                {/* <Route path='/about-us' element={<AboutUs />} /> */}
+                <Route path='/blog' element={<Blog />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                {/* <Route path='/logout' element={<Logout />} /> */}
+                {/* <Route path='/profile' element={<Profile />} /> */}
+                {/* <Route path='/shop' element={<ShopCart />} /> */}
+                <Route path='*' element={<SomethingWentWrong />} />
+            </Routes>
+            {/* <Toasty /> */}
+            {/* </ErrorProvider> */}
+            <ToastContainer
+                position='top-right'
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme='dark'
+            />
             <Footer />
         </>
     );
