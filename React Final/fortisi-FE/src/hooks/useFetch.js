24aafa394c5c921b016f6a443335530dd3b6534f
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-11-30 15:52:04 +0200

    reworked a bit, above version is the normal one, below is the one with "throttling" for some spinner action

--- React Final/fortisi-FE/src/hooks/useFetch.js
+++ React Final/fortisi-FE/src/hooks/useFetch.js
@@ -1,28 +1,57 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react';
 
 export const useFetch = (service, params) => {
-    const [isLoading, setIsLoading] = useState(false)
-    const [errors, setErrors] = useState()
-    const [data, setData] = useState()
-
-
-    const fetchData = useCallback(async () => {
-        setIsLoading(true)
-        try {
-            const result = await service(params)
-            setIsLoading(false)
-            setData(result)
-        } catch (err) {
-            setErrors(err.message)
-        } finally {
-            setIsLoading(false)
-        }
-    }, [service, params])
+    const [isLoading, setIsLoading] = useState(false);
+    const [errors, setErrors] = useState();
+    const [data, setData] = useState();
 
     useEffect(() => {
-        fetchData()
-    }, [fetchData])
+        const fetchData = async () => {
+            setIsLoading(true);
+
+            try {
+                const result = await service(params);
+                setData(result);
+            } catch (err) {
+                setErrors(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [service, params]);
+
+    return { isLoading, errors, data };
+};
 
 
-    return { isLoading, errors, data }
-}
+//Use this for throttling to see the loading spinner.
+
+// export const useFetch = (service, params) => {
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [errors, setErrors] = useState();
+//     const [data, setData] = useState();
+
+//     useEffect(() => {
+//         setIsLoading(true);
+//         const timeout = setTimeout(() => {
+//             const fetchData = async () => {
+//                 try {
+//                     const result = await service(params);
+//                     setData(result);
+//                 } catch (err) {
+//                     setErrors(err.message);
+//                 } finally {
+//                     setIsLoading(false);
+//                 }
+//             };
+//             fetchData();
+//         }, Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000)
+//         return (() => {
+//             clearTimeout(timeout)
+//         })
+//     }, [service, params]);
+
+//     return { isLoading, errors, data };
+// };
