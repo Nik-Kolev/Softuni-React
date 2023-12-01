bb555ac70d11acc6503a4a1279440fc79571090a
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-01 14:00:18 +0200

    replaced custom notification with react-simple-toast for better user XP and mine aswell :]

--- React Final/fortisi-FE/src/App.jsx
+++ React Final/fortisi-FE/src/App.jsx
@@ -5,36 +5,40 @@ import Blog from './components/Blog/Blog';
 import Register from './components/Authentication/Register/Register';
 import Login from './components/Authentication/Login/Login';
 import SomethingWentWrong from './components/Home/404/404';
-import { NotificationProvider } from './context/NotificationContext';
 import { UserProvider } from './context/UserContext';
 import CatalogOptions from './components/Catalog/CatalogOptions/CatalogOptions';
 import CreateProduct from './components/Products/CreateProducts/CreateProduct';
 import EditProduct from './components/Products/EditProducts/EditProduct';
 import CategoryList from './components/Catalog/CategoryList/CategoryList';
 import ProductDetails from './components/Catalog/ProductDetails/ProductDetails';
-import Toasty from './components/Home/ToastNotifications/Toasty';
 import Footer from './Components/Footer/Footer';
+import { toastConfig } from 'react-simple-toasts';
+
 function App() {
+    toastConfig({
+        duration: 2000,
+        offsetY: 120,
+        position: 'top-right',
+        maxVisibleToasts: 3,
+        render: (message) => <b className='my-toast'>{message}</b>,
+    });
     return (
         <>
-            <NotificationProvider>
-                <UserProvider>
-                    <NavigationBar />
-                    <Routes>
-                        <Route path='/' element={<Carousel />} />
-                        <Route path='/catalog' element={<CatalogOptions />} />
-                        <Route path='/catalog/:category' element={<CategoryList />} />
-                        <Route path='/catalog/:category/:id' element={<ProductDetails />} />
-                        <Route path='/blog' element={<Blog />} />
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/register' element={<Register />} />
-                        <Route path='/create-product' element={<CreateProduct />} />
-                        <Route path='/edit-product/:id' element={<EditProduct />} />
-                        <Route path='*' element={<SomethingWentWrong />} />
-                    </Routes>
-                    <Toasty />
-                </UserProvider>
-            </NotificationProvider>
+            <UserProvider>
+                <NavigationBar />
+                <Routes>
+                    <Route path='/' element={<Carousel />} />
+                    <Route path='/catalog' element={<CatalogOptions />} />
+                    <Route path='/catalog/:category' element={<CategoryList />} />
+                    <Route path='/catalog/:category/:id' element={<ProductDetails />} />
+                    <Route path='/blog' element={<Blog />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/create-product' element={<CreateProduct />} />
+                    <Route path='/edit-product/:id' element={<EditProduct />} />
+                    <Route path='*' element={<SomethingWentWrong />} />
+                </Routes>
+            </UserProvider>
             <Footer />
         </>
     );
