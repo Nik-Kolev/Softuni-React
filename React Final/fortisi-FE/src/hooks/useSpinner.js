1d586e7f927f8321643ba2e76b2ac0ec4963debc
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-01 14:08:54 +0200

    useSpinner replaces useFetch as m architecture is not suitable for useFetch hook

---
+++ React Final/fortisi-FE/src/hooks/useSpinner.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-useless-catch */
+import { useState, useCallback } from 'react'
+
+export const useSpinner = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    const handleIsLoading = useCallback(async (callBack) => {
+        setIsLoading(true)
+
+        try {
+            const result = await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    callBack().then(resolve).catch(reject);
+                }, 1000);
+            });
+            return result
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoading(false)
+        }
+    }, [])
+
+    return { isLoading, handleIsLoading }
+}
