92051c45355f51344eeec120999a2bea4bb4b41f
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-02 22:31:59 +0200

    logout clears store state

--- React Final/fortisi-FE/src/components/Authentication/Logout/Logout.jsx
+++ React Final/fortisi-FE/src/components/Authentication/Logout/Logout.jsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 import toast from 'react-simple-toasts';
+import { useStoreContext } from '../../../context/StoreContext';
 import { useUserContext } from '../../../context/UserContext';
 
 export default function Logout() {
     const { onLogoutHandler } = useUserContext();
+    const { emptyBasket } = useStoreContext();
 
     const onSubmitHandler = async () => {
         try {
             const user = await onLogoutHandler();
+            emptyBasket();
             toast(user);
         } catch (err) {
             toast(err.message);
