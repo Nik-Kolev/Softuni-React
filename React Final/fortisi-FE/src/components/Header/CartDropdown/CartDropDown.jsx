a8a5fa21e2d3c8d985b55c0820ccbd53f2ef7f64
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-02 16:19:58 +0200

    cart updated with visual number of products inside the basket

--- React Final/fortisi-FE/src/components/Header/CartDropdown/CartDropDown.jsx
+++ React Final/fortisi-FE/src/components/Header/CartDropdown/CartDropDown.jsx
@@ -1,15 +1,22 @@
 import { DropdownMenu } from '../../../hooks/useDropdownMenu';
 import { Link } from 'react-router-dom';
 import './CardDropDown.css';
+import { useStoreContext } from '../../../context/StoreContext';
+
 export default function CartDropDown() {
     const { showDropdown, hideDropdown, isDropdownVisible } = DropdownMenu();
+    const { products } = useStoreContext();
+    const totalProducts = products.length;
 
     return (
         //TODO: Add a summery of the cart if possible
         <li className='user-container' onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
-            <Link to={'/cart'} className='nav-link'>
-                <img src='/src/images/home/cart.svg' />
-            </Link>
+            <div className='wrapper'>
+                <Link to={'/cart'} className='nav-link'>
+                    <img src='/src/images/home/cart.svg' />
+                </Link>
+                <span className='total-products'>{totalProducts}</span>
+            </div>
             {isDropdownVisible && (
                 <div className='dropdown-menu'>
                     <div>Card information such as products or perhaps number of products and total price</div>
