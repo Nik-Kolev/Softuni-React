cd4d06da833ab9925c72ba12415198e1aa26b110
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-03 20:38:32 +0200

    fake borika page for paying

---
+++ React Final/fortisi-FE/src/components/BorikaFake/BorikaFake.jsx
@@ -0,0 +1,34 @@
+import './BorikaFake.css';
+
+import { useStoreContext } from '../../context/StoreContext';
+
+export default function BorikaFake() {
+    const { total } = useStoreContext();
+    return (
+        <div className='borika-payment-mockup'>
+            <div className='payment-form'>
+                <h2>Плащане чрез БОРИКА</h2>
+                <form>
+                    <div className='form-group'>
+                        <label htmlFor='cardNumber'>Номер на карта:</label>
+                        <input type='text' id='cardNumber' name='cardNumber' placeholder='XXXX XXXX XXXX XXXX' />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='cardExpiry'>Валидност:</label>
+                        <input type='text' id='cardExpiry' name='cardExpiry' placeholder='MM/YY' />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='cardCVC'>CVC:</label>
+                        <input type='text' id='cardCVC' name='cardCVC' placeholder='CVC' />
+                    </div>
+                    <div className='total'>
+                        <p>Сума за плащане: {total} лв.</p>
+                    </div>
+                    <button type='submit' className='submit-btn'>
+                        Плати
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
