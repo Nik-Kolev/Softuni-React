b3b06d3bcc6b3ab151e5649cba29e5360a4ca2e1
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-04 18:18:19 +0200

    new footer - cleaner and smaller

--- React Final/fortisi-FE/src/components/Footer/Footer.jsx
+++ React Final/fortisi-FE/src/components/Footer/Footer.jsx
@@ -1,86 +1,53 @@
 import './Footer.css';
+
 import { Link } from 'react-router-dom';
 export default function Footer() {
     return (
         <footer className='footer-section'>
-            <div className='container relative'>
-                <div className='sofa-img'>
-                    <img src='/src/images/home/sofa.png' alt='Image' className='img-fluid' />
+            <div className='container'>
+                <div className='footer-content'>
+                    <p>
+                        Открийте елегантността с ексклузивната колекция мебели на <span className='brand-text'> Fortisi</span>. Изработени
+                        за комфорт, проектирани за живот.
+                    </p>
+                    <ul className='footer-social-links'>
+                        <li>
+                            <a href='#' className='social-link'>
+                                <span className='fa fa-brands fa-facebook-f'></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#' className='social-link'>
+                                <span className='fa fa-brands fa-twitter'></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#' className='social-link'>
+                                <span className='fa fa-brands fa-instagram'></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#' className='social-link'>
+                                <span className='fa fa-brands fa-linkedin'></span>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <div className='row g-5 mb-5'>
-                    <div className='col-lg-4'>
-                        <div className='mb-4 footer-logo-wrap'>
-                            <Link to={'/'} className='brand-name'>
-                                <p>Fortisi</p>
-                                <img src='/src/images/home/ftslogo.png' className='small-logo' />
-                            </Link>
-                        </div>
-                        <p className='mb-4'>
-                            {/* TODO: Add something here */}
-                            Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
-                            velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant
-                        </p>
-                        {/* TODO: Add links */}
-                        <ul className='list-unstyled custom-social'>
-                            <li>
-                                <a href='#'>
-                                    <span className='fa fa-brands fa-facebook-f'></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <span className='fa fa-brands fa-twitter'></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <span className='fa fa-brands fa-instagram'></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <span className='fa fa-brands fa-linkedin'></span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className='col-lg-8'>
-                        <div className='row links-wrap'>
-                            <div className='col-6 col-sm-6 col-md-3'>
-                                <ul className='list-unstyled'>
-                                    <li>
-                                        <a href='#'>About us</a>
-                                    </li>
-                                    <li>
-                                        <a href='#'>Services</a>
-                                    </li>
-                                    <li>
-                                        <a href='#'>Blog</a>
-                                    </li>
-                                    <li>
-                                        <a href='#'>Contact us</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className='col-6 col-sm-6 col-md-3'>
-                                <ul className='list-unstyled'>
-                                    <li>
-                                        <a href='#'>Jobs</a>
-                                    </li>
-                                    <li>
-                                        <a href='#'>Our team</a>
-                                    </li>
-                                    <li>
-                                        <a href='#'>Leadership</a>
-                                    </li>
-                                    <li>
-                                        <a href='#'>Privacy Policy</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                <div className='footer-nav'>
+                    <ul className='footer-links'>
+                        <li>
+                            <a href='#'>About us</a>
+                        </li>
+                        <li>
+                            <a href='#'>Catalog</a>
+                        </li>
+                        <li>
+                            <a href='#'>Blog</a>
+                        </li>
+                        <li>
+                            <a href='#'>Contact us</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </footer>
