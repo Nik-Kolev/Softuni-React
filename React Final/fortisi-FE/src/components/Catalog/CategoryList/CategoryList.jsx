d6709b4eece162d3ec6975fc348441f477b5c166
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-05 20:04:13 +0200

    pagination on /catalog/category page

--- React Final/fortisi-FE/src/components/Catalog/CategoryList/CategoryList.jsx
+++ React Final/fortisi-FE/src/components/Catalog/CategoryList/CategoryList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import toast from 'react-simple-toasts';
 
 import { useSpinner } from '../../../hooks/useSpinner';
@@ -9,17 +9,27 @@ import CategoryListCard from './CategoryListCard';
 
 export default function CategoryList() {
     const { category } = useParams();
-    const [data, setData] = useState();
+    const [searchParams] = useSearchParams();
     const { isLoading, handleIsLoading } = useSpinner();
+    const [data, setData] = useState();
+    const [totalPages, setTotalPages] = useState(0);
+    const navigateTo = useNavigate();
+    const page = parseInt(searchParams.get('page'), 10) || 1;
 
     useEffect(() => {
         handleIsLoading(async () => {
-            const result = await getProductByCategory(category);
-            setData(result);
+            const result = await getProductByCategory(category, page);
+            setData(result.products);
+            setTotalPages(result.totalPages);
+            if (page > result.totalPages) {
+                navigateTo(`?page=${result.totalPages}`);
+            }
         }).catch((err) => {
             toast(err.message);
         });
-    }, [category, handleIsLoading]);
+    }, [category, handleIsLoading, page, navigateTo]);
+
+    const numberOfPages = [...Array(totalPages + 1).keys()].slice(1);
 
     return (
         <>
@@ -32,6 +42,31 @@ export default function CategoryList() {
                             <div className='row'>{data && data.map((x) => <CategoryListCard key={x._id} {...x} />)}</div>
                         </div>
                     </div>
+                    {totalPages != 1 && (
+                        <div className='pagination-container'>
+                            <ul className='pagination'>
+                                {page !== 1 && (
+                                    <li>
+                                        <Link to={`?page=${page - 1}`} style={{ fontSize: '24px' }}>
+                                            &laquo;
+                                        </Link>
+                                    </li>
+                                )}
+                                {numberOfPages.map((pageNumber) => (
+                                    <li key={pageNumber} className={page === pageNumber ? 'active' : ''}>
+                                        <Link to={`?page=${pageNumber}`}>{pageNumber}</Link>
+                                    </li>
+                                ))}
+                                {totalPages !== page && (
+                                    <li>
+                                        <Link to={`?page=${page + 1}`} style={{ fontSize: '24px' }}>
+                                            &raquo;
+                                        </Link>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    )}
                 </div>
             )}
         </>
