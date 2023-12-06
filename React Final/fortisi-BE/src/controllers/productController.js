fddf25fb7ef5f655b513892f3d854440ea904e62
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-06 20:38:30 +0200

    updated productController for pagination on category

--- React Final/fortisi-BE/src/controllers/productController.js
+++ React Final/fortisi-BE/src/controllers/productController.js
@@ -3,21 +3,33 @@ const productModel = require('../models/Product')
 
 productController.post('/create', async (req, res) => {
     const { productType, name, quantity, category, price, discount, imageUrl, ...details } = req.body
-    const owner = req?.user._id
     try {
+        const owner = req?.user?._id
         const product = await productModel.create({ productType, name, quantity, category, price, discount, imageUrl, ...details, owner })
         res.status(200).json(product)
     } catch (err) {
+        console.log(err.message)
         console.log(err)
     }
 
 })
 
 productController.get('/getByCategory/:category', async (req, res) => {
-    const { category } = req.params
     try {
-        const products = await productModel.find({ category })
-        res.status(200).json(products)
+        const { category } = req.params
+        const page = Number(req.query.page)
+        const limit = 9
+        const skip = (page - 1) * limit
+
+        const totalProducts = await productModel.countDocuments({ category })
+        const totalPages = Math.ceil(totalProducts / limit)
+        const products = await productModel.find({ category }).limit(limit).skip(skip)
+
+        const data = {
+            products,
+            totalPages,
+        }
+        res.status(200).json(data)
     } catch (err) {
         console.log(err)
     }
@@ -42,6 +54,8 @@ productController.get('/delete/:id', async (req, res) => {
 productController.post('/editProductById/:id', async (req, res) => {
     const { id } = req.params
     const data = req.body
+    console.log(data)
+    console.log(id)
     const updatedProduct = await productModel.findOneAndUpdate({ _id: id }, data, { new: true })
     res.status(200).json(updatedProduct)
 })
