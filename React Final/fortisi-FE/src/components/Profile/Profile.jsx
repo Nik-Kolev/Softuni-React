025374d09916fb62b35b646b89ed77576ae0d1df
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-06 20:40:10 +0200

    main Profile component reworked, more sub components to be added

--- React Final/fortisi-FE/src/components/Profile/Profile.jsx
+++ React Final/fortisi-FE/src/components/Profile/Profile.jsx
@@ -1,9 +1,48 @@
-import { Link } from 'react-router-dom';
+import './Profile.css';
+
+import { useState } from 'react';
+
+import { useUserContext } from '../../context/UserContext';
+import ChangePassword from './User/ChangePassword/ChangePassword';
+import UserProfile from './User/MyProfile/UserProfile';
 
 export default function Profile() {
+    const [currentComponent, setCurrentComponent] = useState('Admin');
+
+    const { user } = useUserContext();
+    console.log(user);
+    const renderComponent = () => {
+        switch (currentComponent) {
+            case 'changePassword':
+                return <ChangePassword />;
+            default:
+                return <UserProfile />;
+        }
+    };
+
+    const handleClick = (e, componentName) => {
+        e.preventDefault();
+        setCurrentComponent(componentName);
+    };
+
     return (
-        <Link to={'/profile'} className='dropdown-link'>
-            Профил
-        </Link>
+        <div className='main-content'>
+            <div className='profile-container'>
+                <nav className='profile-navbar'>
+                    <ul>
+                        <li>
+                            <div onClick={(e) => handleClick(e, 'userProfile')}>Моят профил</div>
+                        </li>
+                        <li>
+                            <div onClick={(e) => handleClick(e, 'changePassword')}>Смяна на парола</div>
+                        </li>
+                        <li>
+                            <div onClick={(e) => handleClick(e, 'Test')}>Адрес за доставка</div>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='container-right'>{renderComponent()}</div>
+            </div>
+        </div>
     );
 }
