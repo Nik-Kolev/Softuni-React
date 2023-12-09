ccad23420e5e645e49e81d3ffe71f74fea0d46d9
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-09 08:38:23 +0200

    moved picture to more appropriate folder

--- React Final/fortisi-FE/src/components/Blog/Blog.jsx
+++ React Final/fortisi-FE/src/components/Blog/Blog.jsx
@@ -3,7 +3,7 @@ import './Blog.css';
 export default function Blog() {
     return (
         <div className='main-content'>
-            <img src='/src/images/aboutUs/underdevelopment.gif' alt='development' className='development-img' />
+            <img src='/src/images/development/underdevelopment.gif' alt='development' className='development-img' />
         </div>
     );
 }
