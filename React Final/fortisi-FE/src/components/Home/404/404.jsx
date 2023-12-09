60555bedfd78164d632cc53840ee1a10b149bca9
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-12-09 08:28:52 +0200

    translated to bulgarian and updated with link

--- React Final/fortisi-FE/src/components/Home/404/404.jsx
+++ React Final/fortisi-FE/src/components/Home/404/404.jsx
@@ -1,19 +1,22 @@
 import './404.css';
+
+import { Link } from 'react-router-dom';
+
 export default function SomethingWentWrong() {
     return (
-        <div className='notFound'>
-            <div className='notFound_Left'>
-                <figure>
-                    <img src='/src/images/home/image404.png' alt='error404' />
-                </figure>
-            </div>
-            <div className='notFound_Right'>
-                <p className='notFound_Error'>ERROR 404</p>
-                <p className='notFound_Big'>Something is missing?</p>
-                <p className='notFound_Descr'>The page you are looking for is not found or the product is out of stock.</p>
-                <a id='asd' href='/'>
-                    Back to Home Page
-                </a>
+        <div className='main-content'>
+            <div className='notFound'>
+                <div className='notFound_Left'>
+                    <figure>
+                        <img src='/src/images/home/image404.png' alt='error404' />
+                    </figure>
+                </div>
+                <div className='notFound_Right'>
+                    <p className='notFound_Error'>ERROR 404</p>
+                    <p className='notFound_Big'>Нещо Липсва?</p>
+                    <p className='notFound_Descr'>Страницата, която се опитвате да достъпите липсва или не е намерена.</p>
+                    <Link to={'/'}>Обратно към началната страница.</Link>
+                </div>
             </div>
         </div>
     );
